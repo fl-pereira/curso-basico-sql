cbb0af36ea3a1fe186f4776a0eb61c0502abe5e0
fl-pereira <fpereira.media@gmail.com>
2023-01-12 11:53:26 -0300

    sync

---
+++ insert.sql
@@ -0,0 +1,8 @@
+-- INSERÇÃO DE DADOS
+INSERT INTO 
+	users (first_name, last_name, email, password_hash)
+VALUES
+	("Daphne", "Correa", "mail1@mail.com", "a_hash"),
+    ("Daphne", "Correa", "mail2@mail.com", "a_hash"),
+    ("Daphne", "Correa", "mail3@mail.com", "a_hash"),
+    ("Daphne", "Correa", "mail4@mail.com", "a_hash");
