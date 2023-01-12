74e2681f29d0f4761a1c841490d0a034056630ec
fl-pereira <fpereira.media@gmail.com>
2023-01-12 12:23:55 -0300

    sync

--- select-where.sql
+++ select-where.sql
@@ -1,2 +1,2 @@
 -- SELEÇÃO DE DADOS COM WHERE
-SELECT first_name AS primeiro_nome, email FROM users u WHERE first_name LIKE 'DAPHNE';
+SELECT first_name AS primeiro_nome, email FROM users u WHERE first_name LIKE 'DAPHNE' AND createdAt < '2024-01-12 00:00:00';
